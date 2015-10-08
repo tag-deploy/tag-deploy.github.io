fb5dfde22f1312a8a51dcd6bd246e0e0742d4285
Alex Sears <alexwsears@gmail.com>
2015-10-08 12:26:13 -0400

    Added contact form

--- js/main.js
+++ js/main.js
@@ -5,4 +5,19 @@
             scrollTop: $(divId).offset().top - 20
         }, 'slow');
     });
+
+    $('#beta-access-form').on('submit', function(e) {
+        e.preventDefault();
+        var formData = $(this).serializeArray();
+        var postData = { email: formData[0]['value'] };
+
+        $.post('http://app.tagdeploy.com/contact', postData, function(res) {
+            console.log(res);
+            var successMessage = $('<p/>', {
+                text: 'Thank you!  We will contact you when there are open spots for the beta version.'
+            });
+
+            $('#beta-access-form-container').empty().append(successMessage);
+        });
+    });
 })(jQuery);
